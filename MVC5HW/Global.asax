401e2d7b257b6c3d5663cbfde110f7b0212b076f
SHA-PAI LI <lisa0621@gmail.com>
2015-11-09 19:39:23 +0800

    Create Project

---
+++ MVC5HW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5HW.MvcApplication" Language="C#" %>
---
+++ MVC5HW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5HW.Startup))]
+namespace MVC5HW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
