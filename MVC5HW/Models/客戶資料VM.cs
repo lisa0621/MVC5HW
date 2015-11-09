ccf8a1dfb110133d9ddc363a195867152cab861d
SHA-PAI LI <lisa0621@gmail.com>
2015-11-09 20:19:53 +0800

    「客戶資料管理」、 「客戶聯絡人管理」與 「客戶銀行帳戶管理」 等簡易 CRUD 功能

---
+++ MVC5HW/Models/客戶資料VM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5HW.Models
+{
+    public class 客戶資料VM
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 客戶名稱 { get; set; }
+
+        [StringLength(8, ErrorMessage = "欄位長度不得大於 8 個字元")]
+        [Required]
+        public string 統一編號 { get; set; }
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 電話 { get; set; }
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
+        public string 傳真 { get; set; }
+
+        [StringLength(100, ErrorMessage = "欄位長度不得大於 100 個字元")]
+        public string 地址 { get; set; }
+
+        [StringLength(250, ErrorMessage = "欄位長度不得大於 250 個字元")]
+        public string Email { get; set; }
+    }
+}
